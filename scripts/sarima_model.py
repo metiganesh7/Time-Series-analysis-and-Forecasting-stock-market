6c9e5ddddcabdf4297bb5f0d768d17b315dd723b
metiganesh7 <metiganesh7@gmail.com>
2025-12-06 14:03:43 +0530

    Update sarima_model.py

--- scripts/sarima_model.py
+++ scripts/sarima_model.py
@@ -1,16 +1,41 @@
-
 import warnings
 warnings.filterwarnings('ignore')
-import joblib, os
+
+import joblib
+import os
+import pandas as pd
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-def train_sarima(train, order=(1,1,1), seasonal_order=(1,1,1,12), save_path='models', name='sarima_model.pkl'):
+
+def train_sarima(train, order=(1,1,1), seasonal_order=(1,1,1,12),
+                 save_path='models', name='sarima_model.pkl'):
+    """
+    Train a SARIMA model on a pandas Series or DataFrame column.
+    """
+
     os.makedirs(save_path, exist_ok=True)
-    model = SARIMAX(train, order=order, seasonal_order=seasonal_order, enforce_stationarity=False, enforce_invertibility=False)
+
+    # If train is a DataFrame, convert to Series
+    if isinstance(train, pd.DataFrame):
+        train = train.iloc[:, 0]
+
+    model = SARIMAX(
+        train,
+        order=order,
+        seasonal_order=seasonal_order,
+        enforce_stationarity=False,
+        enforce_invertibility=False
+    )
+
     res = model.fit(disp=False)
+
     joblib.dump(res, os.path.join(save_path, name))
     return res
 
+
 def forecast_sarima(model_res, steps=30):
+    """
+    Forecast next N time steps using a fitted SARIMA model.
+    """
     pred = model_res.get_forecast(steps=steps).predicted_mean
     return pred
